6fd49ab25a0ec17f7aec5f9ff7c3de2ab31f27cc
David Mudrak <david.mudrak@gmail.com>
2010-10-30 00:08:05 +0200

    MDL-24932 Fixed support for IE in moodleemoticon plugin

--- moodle/plugins/moodleemoticon/js/dialog.js
+++ moodle/plugins/moodleemoticon/js/dialog.js
@@ -1,9 +1,57 @@
 var MoodleEmoticonDialog = {
 
-    insert : function(html) {
-        tinyMCEPopup.editor.execCommand('mceInsertContent', false, html);
-        tinyMCEPopup.close();
+    bookmark : null,
+
+    init : function() {
+        this.bookmark = tinyMCEPopup.editor.selection.getBookmark();
+
+        // register event handlers for the table rows
+        tinymce.each(tinymce.DOM.select('tr.emoticoninfo', document), function(row) {
+
+            tinymce.dom.Event.add(row, 'mouseover', function(e) {
+                this.style.backgroundColor = 'white';
+            }, row);
+
+            tinymce.dom.Event.add(row, 'mouseout', function(e) {
+                this.style.backgroundColor = 'transparent';
+            }, row);
+
+            tinymce.dom.Event.add(row, 'click', function(e) {
+                var matches = /^emoticoninfo emoticoninfo-index-([0-9]+)$/.exec(this.className);
+                if (matches.length != 2) {
+                    // continue with the next row
+                    return true;
+                }
+                var index = matches[1];
+                MoodleEmoticonDialog.insert(index);
+            }, row);
+
+        });
+    },
+
+    insert : function(index) {
+        emoticons = tinyMCEPopup.editor._emoticons;
+        i = 0;
+        for (var emoticon in emoticons) {
+            if (i == index) {
+                tinyMCEPopup.editor.selection.moveToBookmark(this.bookmark);
+                tinyMCEPopup.editor.execCommand('mceInsertContent', false, emoticons[emoticon]);
+                this.bookmark = tinyMCEPopup.editor.selection.getBookmark();
+                tinyMCEPopup.close();
+                return;
+            }
+            i++;
+        }
+    },
+
+    highlight : function(row) {
+        row.style.backgroundColor="white";
+    },
+
+    unhighlight : function(row) {
+        row.style.backgroundColor="transparent";
     }
+
 };
 
 tinyMCEPopup.onInit.add(MoodleEmoticonDialog.init, MoodleEmoticonDialog);
